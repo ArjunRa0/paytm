b643f9ad0222d38dab422ae2f5d1a6d7d8e924d0
ArjunRa0 <karanarjunrao555@gmail.com>
2024-09-21 21:01:30 -0700

    checkpoint6

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,6 +4,7 @@ const { User } = require("../db");
 const jwt = require("jsonwebtoken");
 const JWT_SECRET = require("../config");
 const router = express.Router();
+const {authMiddleware} = require("../middleware");
 
 const signupSchema = zod.object({
     username: zod.string().email(),
@@ -26,6 +27,7 @@ router.post("/signup", async (req,res) => {
         })
     }
 
+    // Check if the user already exists 
     const user = await User.findOne({
         username: body.username
     })
@@ -36,6 +38,7 @@ router.post("/signup", async (req,res) => {
         })
     }
 
+    // Hash the password
     const hashedPassword = await bcrypt.hash(body.password, 10); // 10 is the salt rounds for bcrypt
 
     const dbUser = await User.create({
@@ -89,6 +92,55 @@ router.post("/signin", async (req, res) => {
         token
     });
 });
+
+const updateBody = zod.object({
+    password: zod.string().optional(),
+    firstName: zod.string().optional(),
+    lastName: zod.string().optional()
+
+})
+
+router.put("/",authMiddleware, async (req,res) => {
+    const { success } = updateBody.safeParse(req.body)
+
+    if (!success) {
+        res.status(411).json({
+            message: "Error while updating information"
+        })
+    }
+    await User.updateOne({_id: req.userId },req.body);
+
+    res.json({
+        message: "Updated successfully"
+    })
+})
+
+router.get("/bulk", async (req, res) => {
+    const filter = req.query.filter || "";
+
+    const users = await User.find({
+        $or: [{
+            firstName: {
+                "$regex": filter
+            }
+        }, {
+            lastName: {
+                "$regex": filter
+            }
+        }]
+    })
+
+    res.json({
+        user: users.map(user => ({
+            username: user.username,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            _id: user._id
+        }))
+    })
+})
+
+
 
 
 
